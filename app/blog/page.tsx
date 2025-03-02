f32700bbb3080226f63940266de728c7b7ec7cd4
sundaray <pgcim14.hemant@spjimr.org>
2025-03-02 10:33:57 +0530

    added the blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+export default async function Blog() {
+  return (
+    <div className="mx-auto max-w-xl px-4">
+      <h2 className="text-secondary-foreground mb-2 text-4xl font-semibold tracking-tight">
+        Blog
+      </h2>
+      <p className="text-muted-foreground font-medium">Insights on AUTOSAR</p>
+    </div>
+  );
+}
